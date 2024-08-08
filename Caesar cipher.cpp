7a4247ce9cf6f2c12f7c309daafdadc1e08fe948
BONK [Junsious] <114828272+OpenSourceIsllife@users.noreply.github.com>
2024-08-08 20:37:37 +0300

    Update Caesar cipher.cpp

--- Caesar cipher.cpp
+++ Caesar cipher.cpp
@@ -1,34 +1,54 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 
 std::string caesarDecrypt(const std::string& text, int shift) {
-    std::string decrypted = "";
+    std::string decrypted = ""; 
 
+    
     for (char ch : text) {
+        
         if (isalpha(ch)) {
+            
             char base = islower(ch) ? 'a' : 'A';
+            
             decrypted += static_cast<char>(
                 (static_cast<int>(ch) - static_cast<int>(base) - shift + 26) % 26 + static_cast<int>(base)
                 );
-
         }
         else {
+            
             decrypted += ch;
         }
     }
-    return decrypted;
+
+    return decrypted; 
 }
 
 int main() {
-    std::string encryptedText;
-    int shift;
+    std::string encryptedText; 
+    int shift; 
 
-    std::cout << "Enter the encrypted text: ";
-    std::getline(std::cin, encryptedText);
-    std::cout << "Enter an offset (for example, 3):";
-    std::cin >> shift;
+    while (true) { 
+        
+        std::cout << "Enter the encrypted text (or 'exit' to exit): ";
+        std::getline(std::cin, encryptedText); 
 
-    std::string decryptedText = caesarDecrypt(encryptedText, shift);
-    std::cout << "Decrypted text:" << decryptedText << std::endl;
-    return 0;
+        
+        if (encryptedText == "exit") {
+            break; 
+        }
+
+        // Запрашиваем у пользователя ввод значения сдвига
+        std::cout << "Enter an offset (for example, 3): ";
+        std::cin >> shift; 
+        std::cin.ignore(); 
+
+        
+        std::string decryptedText = caesarDecrypt(encryptedText, shift);
+
+        // Выводим расшифрованный текст на экран
+        std::cout << "Decrypted text:" << decryptedText << std::endl;
+    }
+
+    return 0; 
 }
